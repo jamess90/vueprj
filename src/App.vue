fae5a1d2fe2cf19ca86fc30acb5429934fa8cf7a
hyeongjun <jamess90@naver.com>
2020-03-25 10:28:15 +0900

    디렉토리 구조 변경2

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div slot="body">
+      <div id="topwrap" style="height:88px;">
+        <div style="position:relative;height:50px;width:100%;background-color:#b48664;border-bottom:3px solid #ce9972;">
+          <div class="div_banner" style="position:relative;height:50px;width:700px;margin:0px auto;overflow:hidden;text-align:center;">
+            <ul id="div_banner_list" style="margin:0px auto;padding:0px;width:8000px;">
+              <li><div class="div_banner_item"><img src="./assets/logo.png" alt="농협카드"> "인터컨티넨탈 다낭 선페닌슐라 리조트 " <span style="padding-left: 5px;">3월 할인</span></div></li>
+            </ul>
+          </div>
+        </div>
+        <div class="clear"></div>
+        <div id="navgation">
+          <div id="logo" onclick="location.href='/hotel/?hotel=306'"></div>
+          <div id="menu">
+            <ul>
+              <li class="promo"><a href="/hotelonly"><i class="fab fa-product-hunt"></i> 호텔예약</a></li>
+              <li><a href="/hotel/mypage.php?hotel=306">예약확인</a></li>
+              <li><a href="/hotel/board.php?hotel=306&amp;ts=qna">온라인문의</a></li>
+              <li><a href="/hotel/board.php?hotel=306&amp;ts=qna_cop">기업/단체문의</a></li>
+              <!-- <li><a href="board.php?ts=faq&category=1&hotel=306">FAQ</a></li> -->
+              <li style="cursor:pointer" onclick="window.open('https://admin8.kcp.co.kr/html/popup/thismonth/html/kcp_pop_up.html','','toolbars=no,scrollbars=yes,width=600,height=700')"><i class="fas fa-credit-card"></i> 신용카드 무이자할부</li>
+              <li class="promo"><a href="board.php?hotel=306&amp;ts=event_list"><i class="fab fa-product-hunt"></i> 프로모션</a></li>
+              <li onclick="javascript:btnScroll.scrollControl.goTop();" id="morehotel" style="color:#fff; margin:0px 10px 0px 10px">다른호텔보기</li>
+            </ul>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+      <div id="app" class="app">
+        <router-view> </router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fn_setCount () {
+      // eslint-disable-next-line no-return-assign
+      return this.store.test = 'test'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import router from './router'
+import store from './store'
+import $ from 'jquery'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  $,
+  render: h => h(App)
+})
